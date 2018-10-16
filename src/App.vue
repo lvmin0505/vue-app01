fdaa4fe5d4bf966e7828ca3ad82494f177e5452b
lvmin0505 <1401228435@qq.com>
2018-10-16 17:15:05 +0800

    1

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <!-- <img src="./assets/logo.png">
+    <HelloWorld/>
+    <Bind/> -->
+    <User/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+import Bind from './components/Bind'
+import User from './components/User'
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    Bind,
+    User
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/User.vue
@@ -0,0 +1,83 @@
+<template>
+	<div class="user">
+		<h2>用户管理 </h2>
+
+		<div class="btn">
+			<button>添加</button>
+		</div>
+
+		<table class="table">
+			<thead>
+				<tr>
+					<th>编号</th>
+					<th>姓名</th>
+					<th>性别</th>
+					<th>生日</th>
+					<th>入职时间</th>
+					<th>身份</th>
+					<th>操作</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<td>
+						<input type="checkbox">
+					</td>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td>
+						<a href="javascript:void(0);">删除</a>
+						<a href="javascript:void(0);">修改</a>
+					</td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
+</template>
+
+	<script>
+		import {baseUrl} from '@/config/url'
+		export default {
+			data(){
+				return {
+					msg:'hello',
+				
+				}
+			},
+			methods:{
+				loadUsers(successHandler){
+					$.get(baseUrl+"/user/findAll",({status,data})=>{
+						if(status == 200){
+							successHandler(data);
+						} else {
+							alert('加载失败');
+						}
+					})
+				}
+			},
+			
+			beforeMount(){
+				this.loadUsers((data)=>{
+					console.log(data);
+				});
+			}
+			
+		}
+	</script>
+
+<style>
+	.btn {
+		margin: 10px 0;
+	}
+	.table {
+		width: 90%;
+		border-collapse: collapse;
+	}
+	.table td, .table th {
+		border: 1px solid #ccc;
+		line-height: 1.6em;
+	}
+</style>
